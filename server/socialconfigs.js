21c3152bb377cc306db0699e1075779026914cd0
orozcorp <orozcorp@gmail.com>
2015-05-09 20:24:04 -0500

    ya funciona Facebook

--- server/socialconfigs.js
+++ server/socialconfigs.js
@@ -1,10 +1,18 @@
-ServiceConfiguration.configurations.upsert(
-  { service: "facebook" },
-  {
-    $set: {
-      loginStyle: "popup",
-    clientId: '594623523973899',
-    secret: '9d60d2518a5b1732963ecfe15375e586'
+Meteor.startup(function () {    
+  environment = process.env.NODE_ENV;
+
+  try {
+    if (environment == 'production') {
+      ServiceConfiguration.configurations.remove({
+        service: "facebook"
+      });
+      ServiceConfiguration.configurations.insert({
+        "service" : "facebook",
+        "appId" : "594623523973899",
+        "secret" : "9d60d2518a5b1732963ecfe15375e586"
+      });
     }
+  } catch(error) {
+    console.log(error.message);
   }
-);
+});
